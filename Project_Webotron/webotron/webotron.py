5a51ce5582a2dd105da60bbe315761279f279352
Shrikar Archak <shrikar84@gmail.com>
2018-07-22 11:02:19 -0700

    Add profile param

--- Project_Webotron/webotron/webotron.py
+++ Project_Webotron/webotron/webotron.py
@@ -8,14 +8,18 @@ import boto3
 import click 
 from bucket import BucketManager
 
-session = boto3.Session(profile_name='pythonAutomation')
-bucket_manager = BucketManager(session)
-
-
+session = None
+bucket_manager = None
 @click.group()
-def cli():
+@click.option('--profile', default=None, help="Provide user profile")
+def cli(profile):
     "Webotron deploys websites to AWS"
-    pass    
+    global session, bucket_manager
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile        
+    session = boto3.Session(**session_cfg)
+    bucket_manager = BucketManager(session)       
 
 @cli.command('list-buckets')
 def list_buckets():
