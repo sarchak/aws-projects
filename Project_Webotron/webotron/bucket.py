8fe56a213e195098989f95ca35907f14da681c7a
Shrikar Archak <shrikar84@gmail.com>
2018-07-24 23:09:49 -0700

    Allow skipping files from syncing when etags match

--- Project_Webotron/webotron/bucket.py
+++ Project_Webotron/webotron/bucket.py
@@ -3,13 +3,22 @@ from botocore.exceptions import ClientError
 import mimetypes
 from pathlib import Path
 import util
+from hashlib import md5
+from functools import reduce
+import boto3
 
 """ Class for s3 buckets."""
 class BucketManager: 
     """Manage an S3 Bucket."""
+    CHUNK_SIZE = 8388608
     def __init__(self, session):
         """Create a bucket Manager object"""
         self.s3 = session.resource('s3')
+        self.manifest = {}
+        self.transfer_config =  boto3.s3.transfer.TransferConfig(
+            multipart_chunksize = self.CHUNK_SIZE,
+            multipart_threshold = self.CHUNK_SIZE
+        )
 
     def all_buckets(self):
         """Get an iterator for all buckets."""
@@ -64,13 +73,47 @@ class BucketManager:
         })
 
     @staticmethod
-    def upload_file(bucket, path, key):
+    def hash_data(data):
+        hash = md5()
+        hash.update(data)
+        return hash
+
+    def gen_etag(self, path):
+        hashes = []
+        with open(path, 'rb') as f:
+            while(True):
+                data = f.read(self.CHUNK_SIZE)
+                if not data:
+                    break
+                hashes.append(self.hash_data(data))
+            if not hashes:
+                return
+            elif len(hashes) == 1:
+                return '"{}"'.format(hashes[-1].hexdigest())
+            else:
+                hash = self.hash_data(reduce(lambda x, y: x+y, (h.digest() for h in hashes)))
+                return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
+
+    def load_manifest(self, bucket):
+         paginator = self.s3.meta.client.get_paginator('list_objects_v2')
+         for page in paginator.paginate(Bucket=bucket.name):
+            for obj in page.get('Contents', []):
+                self.manifest[obj['Key']] = obj['ETag'] 
+
+
+    def upload_file(self, bucket, path, key):
         """Upload path to s3 bucket at key."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-        return bucket.upload_file(path, key, ExtraArgs={'ContentType': content_type})
+        etag = self.gen_etag(path)
+        if self.manifest.get(key, '') == etag:
+            print("Skipping {}, etags match".format(key))
+            return
+
+        return bucket.upload_file(path, key, ExtraArgs={'ContentType': content_type}, Config=self.transfer_config)
 
     def sync(self, pathname, bucket_name):
         bucket = self.s3.Bucket(bucket_name)
+        self.load_manifest(bucket)
         root = Path(pathname).expanduser().resolve()
         def handle_directory(target):
             for p in target.iterdir():
@@ -78,5 +121,5 @@ class BucketManager:
                 if p.is_file():
                     print("Path: {} \t Key: {}".format(p, p.relative_to(root)))
                     self.upload_file(bucket, str(p), str(p.relative_to(root)))
-    
+
         handle_directory(root)
